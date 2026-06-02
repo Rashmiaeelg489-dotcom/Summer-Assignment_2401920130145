739e56e4e44af87d340af543a98b4399d49eee26
Rashmi Anil Gupta <aneelg489@gmail.com>
2026-06-02 22:53:49 +0530

    done question 3 of day 2

---
+++ week_1/Day_2/max_avg_subarray.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+    long long sum=0;
+    for(int i=0;i<k;i++) 
+    {
+        sum=sum+nums[i];
+    }
+    long long maxSum=sum;
+    for(int i=k;i<nums.size();i++) 
+    {
+        sum=sum+nums[i];
+        sum=sum-nums[i-k];
+        maxSum=max(maxSum,sum);
+    }
+    return(double)maxSum/k;
+    }
+};
