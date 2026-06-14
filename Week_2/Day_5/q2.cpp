f978ae516d08171d211d99e3438e2044d145867d
Rashmi Anil Gupta <aneelg489@gmail.com>
2026-06-14 12:37:10 +0530

    done

--- Week_2/Day_5/q2.cpp
+++ Week_2/Day_5/q2.cpp
@@ -1,6 +1,35 @@
 #include<string>
 #include<vector>
-#include<unordered_map>
-#include<algorithm>
 using namespace std;
 
+class Solution {
+public:
+    int compress(vector<char>& chars){
+
+        vector<char>ans;
+        int n=chars.size();
+
+        for(int i=0;i<n;)
+        {
+            char ch=chars[i];
+            int cnt=0;
+
+            while(i<n && chars[i]==ch) 
+            {
+                cnt++;
+                i++;
+            }
+
+            ans.push_back(ch);
+
+            if(cnt>1)
+            {
+                string s=to_string(cnt);
+                for(char c:s)
+                    ans.push_back(c);
+            }
+        }
+        chars=ans;
+        return chars.size();
+    }
+};
